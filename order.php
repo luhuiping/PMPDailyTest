2caa646a8e9cabc8beae71beed7ad1c8d0293c20
lhp <2275410489@qq.com>
2019-01-15 08:56:00 +0800

    排序

---
+++ order.php
@@ -0,0 +1,16 @@
+<?php 
+$arr_A=[1,3,6,9];
+$arr_B=[2,4,5,8,20];
+$data=count($arr_A);
+$str=count($arr_B);
+var_dump($data);
+var_dump($str);
+$len=$data+$str;
+$res=explode($len,string);
+var_dump($res);
+
+//Combine($arr_A,$arr_B);
+
+
+
+ ?>
